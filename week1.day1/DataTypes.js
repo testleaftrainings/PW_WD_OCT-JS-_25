f5ecdc073d97f9639db6344c854b4d84717e9018
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-10-15 12:22:20 +0530

    day01

---
+++ week1.day1/DataTypes.js
@@ -0,0 +1,36 @@
+//Number -> wholes numbers and decimals
+//String -> sequences of characters
+//Boolean -> true or false
+//Null    ->Represents intentional absence of value
+//Undefined -> declared variable but not assigned value
+//BigInt -> used for storing larger number
+//Symbols ->unique anf immutable identifer
+
+
+//variable Declaration - variableName= value
+//VD -> var -> 1995 to 2015
+//variableName -> meaning full name
+//value -> value 
+
+var age=18
+console.log("Dinesh age is : "+age)
+
+var comname="TestLeaf"
+console.log("Compay Name is :"+comname)
+
+var isVisble= true
+console.log(isVisble +" :"+typeof(isVisble))
+
+var n=null
+console.log(n +": "+typeof(n))
+var x
+
+var phno=2345678900975678435678n
+console.log("BigInt :"+phno)
+
+//typeof value - check dataType
+console.log(typeof(x))
+console.log(typeof(phno))
+
+x=45
+console.log(x)
---
+++ week1.day1/FirstCode.js
@@ -0,0 +1,7 @@
+//print the value inside console or output
+console.log("Welcome Playwright WeekDay batch")
+console.log("Week1.day1")
+
+//Create File you use fileName.js
+//execute code -> right on the file -> click on Open Integrated Terminal
+//js -> node fileName.js
